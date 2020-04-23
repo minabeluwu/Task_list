b457f6c10b63958b061a9694ed055c976f58932f
Minabeluwu <minabeluwu@gmail.com>
2020-04-23 13:13:05 -0300

    Updates

---
+++ precache-manifest.c19ec7337829d33c614fe32a9217ce4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d183bccfafdb332ffae678c60f5e2e68",
+    "url": "/taskList/index.html"
+  },
+  {
+    "revision": "e34a8d5d1a30757e4aeb",
+    "url": "/taskList/static/css/main.ac64860d.chunk.css"
+  },
+  {
+    "revision": "8983b17c9b413de2bd7a",
+    "url": "/taskList/static/js/2.3e0e8b80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taskList/static/js/2.3e0e8b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34a8d5d1a30757e4aeb",
+    "url": "/taskList/static/js/main.b18839ac.chunk.js"
+  },
+  {
+    "revision": "43542971cb391f068c9d",
+    "url": "/taskList/static/js/runtime-main.18aed3ba.js"
+  }
+]);
