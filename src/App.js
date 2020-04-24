2e398860b194d2060167bde03c7691206cb1ea99
Minabeluwu <minabeluwu@gmail.com>
2020-04-24 16:29:09 -0300

    improvements

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ class App extends Component {
     todos: ["ejemplo1", "ejemplo2"],
   }
 
+  //borrar todos
   handleClick = (index) => {
     const newToDo = this.state.todos.filter((todo, idx) => {
       return idx !== index
     })
     this.setState({ todos: newToDo })
   }
+
+  // Metodo para imprimir los todos
   handleSubmmit = (e) => {
     e.preventDefault()
+    e.target.reset()
     const { input, todos } = this.state
     const newToDo = todos.concat(input)
 
@@ -26,21 +30,33 @@ class App extends Component {
   render() {
     const todoListElements = this.state.todos.map((item, index) => {
       return (
-        <li
-          onClick={() => this.handleClick(index)}
-          key={index}
-          className="list-item"
-        >
-          {item}
-        </li>
+        <div className="list-container">
+          <div className="box">
+            <div className="list-star"></div>
+            <li
+              onClick={() => this.handleClick(index)}
+              key={index}
+              className="list-item"
+            >
+              {item}
+            </li>
+          </div>
+
+          <input
+            type="checkbox"
+            id="cbox1"
+            value="first_checkbox"
+            className="list-checkbox"
+          />
+        </div>
       )
     })
     return (
       <div className="content">
         <div className="container">
-          <h1 className="title">Task List</h1>
-
-          <form onSubmit={this.handleSubmmit}>
+          <div className="title-img"></div>
+          <p className="paragraph">Escribe tu tarea acá</p>
+          <form onSubmit={this.handleSubmmit.bind(this)} className="form">
             <input
               onChange={(e) => {
                 const { value } = e.target
@@ -48,16 +64,19 @@ class App extends Component {
                   input: value,
                 })
               }}
-              className="input"
+              required
+              className="form-input"
             />
+            <button
+              type="submit"
+              onClick={() => this.addItem}
+              className="form-button"
+            >
+              Crear tarea
+            </button>
           </form>
-          <ul className="list">{todoListElements}</ul>
+          <ul className="list">{todoListElements} </ul>
         </div>
-
-        <img
-          src="https://images.pexels.com/photos/1187079/pexels-photo-1187079.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          className="flower"
-        />
       </div>
     )
   }
